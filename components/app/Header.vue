d79f67d0c92fa21f0c5db3c95d4f1cab2645a2fe
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-27 22:21:42 +0900

    feat: header component add

---
+++ components/app/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="mt-5 mb-4 print:hidden">
+    <div class="container max-w-4xl flex justify-between">
+      <div class="flex items-center">
+        <!-- <div className="mr-4">
+          <Sidebar works="{works}" />
+        </div> -->
+
+        <div class="flex-1">
+          <div class="font-bold">
+            <a href="/">title</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center">
+        <div class="hidden md:block mr-4">
+          <AppPrintButton />
+        </div>
+        <AppThemeButton />
+      </div>
+    </div>
+  </header>
+</template>
