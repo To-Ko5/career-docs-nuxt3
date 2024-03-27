9522bc94eae9b9a0e824a2dbcdedd60f3274c83b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-27 22:21:30 +0900

    feat: print button component add

---
+++ components/app/PrintButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { PrinterIcon } from 'lucide-vue-next'
+
+const clickPrint = () => {
+  window.print()
+}
+</script>
+
+<template>
+  <Button variant="outline" size="icon" @click="clickPrint">
+    <PrinterIcon
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all"
+    />
+    <span class="sr-only">Print</span>
+  </Button>
+</template>
