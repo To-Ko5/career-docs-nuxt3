07d3ecaafc984613817d58c555540ab84be42746
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-28 12:09:04 +0900

    feat theme toggle function add

--- components/app/ThemeButton.vue
+++ components/app/ThemeButton.vue
@@ -1,9 +1,19 @@
-<script setup>
+<script setup lang="ts">
 import { MoonIcon, SunIcon } from 'lucide-vue-next'
+
+const colorMode = useColorMode()
+
+const handleClick = () => {
+  if (colorMode.preference === 'light') {
+    colorMode.preference = 'dark'
+  } else {
+    colorMode.preference = 'light'
+  }
+}
 </script>
 
 <template>
-  <Button variant="outline" size="icon">
+  <Button variant="outline" size="icon" @click="handleClick">
     <SunIcon
       class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
     />
