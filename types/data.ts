4f0478e1e6f79a9b901ce2ef4fb1766f03d44bad
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-28 11:43:22 +0900

    feat: types add

---
+++ types/data.ts
@@ -0,0 +1,24 @@
+import type { LucideIcon } from 'lucide-vue-next'
+
+export type work = {
+  case: number
+  title: string
+  startDate: string
+  endDate: string
+  positions: string[]
+  skills: string[]
+  tools: string[]
+  description: string
+}
+
+export type about = {
+  name: string
+  position: string
+  content: string
+}
+
+export type url_link = {
+  icon: LucideIcon
+  href: string
+  tooltip?: string
+}
