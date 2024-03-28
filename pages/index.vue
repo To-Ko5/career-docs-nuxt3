d374d9faea54c5a5fc555486b545484ef0d849c5
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-28 11:18:48 +0900

    feat: custom layout add

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,9 @@ const test = () => {
 </script>
 
 <template>
-  <Button @click="test">Test</Button>
+  <NuxtLayout name="layout">
+    <main class="container max-w-4xl pb-16 space-y-10">
+      <Button @click="test">Test</Button>
+    </main>
+  </NuxtLayout>
 </template>
