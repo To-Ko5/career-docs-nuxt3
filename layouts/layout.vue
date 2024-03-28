f5287ce6c3d779920186cd4280ce41fc1080bcd4
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-28 11:43:34 +0900

    feat: meta set

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -1,9 +1,24 @@
+<script setup>
+const { title } = defineProps({
+  title: { type: String, required: true },
+  description: { type: String, required: true }
+})
+</script>
+
 <template>
-  <div class="flex flex-col min-h-screen">
-    <AppHeader />
-    <div class="flex-grow">
-      <slot />
+  <div>
+    <Html lang="ja">
+      <Head>
+        <Title>{{ title }}</Title>
+        <Meta name="description" :content="`${description}`" />
+      </Head>
+    </Html>
+    <div class="flex flex-col min-h-screen">
+      <AppHeader />
+      <div class="flex-grow">
+        <slot />
+      </div>
+      <AppFooter />
     </div>
-    <AppFooter />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
-<script setup>
-const test = () => {
-  console.log('ccccc')
-}
+<script setup lang="ts">
+import { site } from '@/data/site_data'
 </script>
 
 <template>
-  <NuxtLayout name="layout">
+  <NuxtLayout
+    name="layout"
+    :title="`${site.title}`"
+    :description="`${site.description}`"
+  >
     <main class="container max-w-4xl pb-16 space-y-10">
       <Button @click="test">Test</Button>
     </main>
