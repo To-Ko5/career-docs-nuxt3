3f3709da0af90ca96f78ad86498d58894914ebe6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-29 10:38:39 +0900

    feat: global css add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  css: ['~/assets/css/global.css'],
   colorMode: {
     classSuffix: ''
   },
