61b6bc32d956f4b0a3a393c97d3938ff2aca3fba
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-29 22:49:57 +0900

    feat: useAsyncData add

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
 import { site } from '@/data/site_data'
+import type { work } from '@/types/data'
+
+const { data: worksData } = await useAsyncData('works', async () => {
+  const { $newtClient } = useNuxtApp()
+  return await $newtClient.getContents<work>({
+    appUid: 'document-site',
+    modelUid: 'work',
+    query: {
+      order: ['case']
+    }
+  })
+})
+
+const works = worksData.value?.items
+
+const { data: aboutDate } = await useAsyncData('about', async () => {
+  const { $newtClient } = useNuxtApp()
+  return await $newtClient.getContents<work>({
+    appUid: 'document-site',
+    modelUid: 'about',
+    query: {
+      select: ['name', 'position', 'content']
+    }
+  })
+})
+
+const about = aboutDate.value?.items
 </script>
 
 <template>
@@ -9,7 +36,8 @@ import { site } from '@/data/site_data'
     :description="`${site.description}`"
   >
     <main class="container max-w-4xl pb-16 space-y-10">
-      <Button @click="test">Test</Button>
+      <!-- <Button @click="test">Test</Button> -->
+      <AppHero />
     </main>
   </NuxtLayout>
 </template>
