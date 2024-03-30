c7deb1a567ed92583022a32307cb7ca4ace6a2d2
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-30 18:18:11 +0900

    feat: nuxt-link set

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import { site } from '@/data/site_data'
+</script>
+
 <template>
   <footer class="p-3 border-t print:hidden">
     <p class="text-center text-sm text-muted-foreground">
-      <a href="/">タイトル</a>
+      <NuxtLink to="/">{{ site.title }}</NuxtLink>
     </p>
   </footer>
 </template>
--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { site } from '@/data/site_data'
+</script>
+
 <template>
   <header class="mt-5 mb-4 print:hidden">
     <div class="container max-w-4xl flex justify-between">
@@ -8,7 +12,7 @@
 
         <div class="flex-1">
           <div class="font-bold">
-            <a href="/">title</a>
+            <NuxtLink to="/">{{ site.title }}</NuxtLink>
           </div>
         </div>
       </div>
