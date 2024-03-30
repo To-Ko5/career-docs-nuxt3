d5bbc41e33cc099afd1d802448d83842664c64d9
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-30 11:13:33 +0900

    feat: date component add

---
+++ components/app/Date.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { date } from '@/data/site_data'
+const dayjs = useDayjs()
+dayjs.locale('ja')
+</script>
+
+<template>
+  <p className="text-right text-sm text-muted-foreground mb-4">
+    <time>{{ dayjs(date.lastUpdatedAt).format('YYYY/MM/DD') }}</time>
+  </p>
+</template>
