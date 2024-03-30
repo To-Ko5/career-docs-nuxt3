6e4a9f1e3eeadcf8c433d07bf98b9f4b56d1aa5f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-30 18:17:57 +0900

    feat: about component add

---
+++ components/app/About.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { content } = defineProps({
+  content: { type: String, required: true }
+})
+</script>
+
+<template>
+  <section>
+    <h2 class="text-xl font-bold mb-2">About</h2>
+    <p
+      class="whitespace-pre-line text-muted-foreground leading-[1.6rem] print:text-sm"
+    >
+      {{ content }}
+    </p>
+  </section>
+</template>
