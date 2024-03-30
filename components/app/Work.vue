1ddba8641d2658612c9cc3ea2f990741ae3bfe76
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-30 18:32:56 +0900

    feat: work relation component add

---
+++ components/app/Work.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { work } from '~/types/data'
+
+const { work, isLink } = defineProps({
+  work: { type: Object as PropType<work>, required: true },
+  isLink: { type: Boolean, default: false }
+})
+</script>
+
+<template></template>
---
+++ components/app/Works.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { work } from '~/types/data'
+
+const { works } = defineProps({
+  works: { type: Array<work>, required: true }
+})
+
+// style at printing
+const printStyle =
+  'print:[&:nth-of-type(2)]:break-inside-avoid print:[&:nth-of-type(3)]:break-inside-avoid'
+</script>
+
+<template>
+  <section>
+    <h2 className="text-xl font-bold mb-4">Works</h2>
+
+    <div className="space-y-8">
+      <div v-for="(work, index) in works" :key="index" :class="`${printStyle}`">
+        <AppWork :work="work" />
+      </div>
+    </div>
+  </section>
+</template>
