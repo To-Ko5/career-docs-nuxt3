c328a621cb5b33617ca4e7f7bbbb9b9b45cf8590
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-01 08:01:26 +0900

    feat: error page add

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { buttonVariants } from '@/components/ui/button'
+const error = useError()
+</script>
+
+<template>
+  <NuxtLayout name="not-found-layout" title="Not Found">
+    <div
+      class="absolute inset-0 flex flex-col gap-4 items-center justify-center"
+    >
+      <div class="text-xl">ページが見つかりませんでした</div>
+      <div class="text-xl">{{ error?.statusCode }}</div>
+      <div class="text-xl">{{ error?.message }}</div>
+      <div>
+        <NuxtLink href="/" :class="buttonVariants({ variant: 'outline' })">
+          Topへ</NuxtLink
+        >
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
