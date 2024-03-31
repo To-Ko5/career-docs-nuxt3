144c08b418aa36360fa822f1e987d207322a8425
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-01 08:01:40 +0900

    feat: not found layout add

---
+++ layouts/NotFoundLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { site } from '@/data/site_data'
+const { title, description } = defineProps({
+  title: { type: String, required: true },
+  description: { type: String, required: false, default: site.description }
+})
+</script>
+
+<template>
+  <div>
+    <Html lang="ja">
+      <Head>
+        <Title>{{ title }}</Title>
+        <Meta name="description" :content="`${description}`" />
+      </Head>
+    </Html>
+    <div class="flex flex-col min-h-screen">
+      <AppHeader />
+      <div class="flex-grow">
+        <slot />
+      </div>
+      <AppFooter />
+    </div>
+  </div>
+</template>
